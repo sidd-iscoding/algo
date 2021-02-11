ee96eb8b6d35e8c4233e897b7d0643651495f141
sidd-iscoding <mailsiddharthbhuyan@gmail.com>
2021-02-11 16:21:21 +0530

    algo section

--- bfs.java
+++ bfs.java
@@ -1,6 +1,14 @@
 class bfs{
+	void bft(node s){
+		visited[]
+	}
+
 	public static void main(String[] args){
-		
+		int[] visited;
+		for(int i=0;i<n;i++){
+			visited[i]=0;
+		}
+		bft(s);
 	}
 
 }
